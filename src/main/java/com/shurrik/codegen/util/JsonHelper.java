17d99a7c0d1dcc1d43b79c1ccc26b27059b461c2
shurrik <darkfores123@gmail.com>
2017-03-30 16:56:51 +0800

    'forbaseboot'

---
+++ src/main/java/com/shurrik/codegen/util/JsonHelper.java
@@ -0,0 +1,31 @@
+package com.shurrik.codegen.util;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.util.List;
+
+
+/**
+ * @author lip 创建于	2012-4-12	下午7:06:52 
+ */
+public class JsonHelper {
+	
+	/**	从json文件读取类对象队列
+	 * @return
+	 */
+	public List<JSONObject> getJsonClassList()
+	{
+		FileHelper fh = new FileHelper();
+		String data= fh.readFileByLines(GlobalVariables.JSON_PATH);
+		System.out.println(data);
+		JSONArray list = JSONArray.fromObject(data);
+		/*Iterator itor = jsonObj.keySet().iterator();
+        while (itor.hasNext()) {
+        	JSONArray entry = (JSONArray) jsonObj.get(itor.next());
+            list.add(entry);
+
+        }  */
+        return list;
+	}
+}
---
+++ src/main/java/com/shurrik/codegen/util/ProjectConfig.java
@@ -0,0 +1,121 @@
+package com.shurrik.codegen.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class ProjectConfig {
+
+	private static ProjectConfig instance = null;
+	public static ProjectConfig getInstance(){
+		if(instance==null)
+		{
+			synchronized (ProjectConfig.class) {
+				if(instance ==null)
+				{
+					instance = new ProjectConfig();
+					instance.loadProperties();
+				}
+			}
+		}
+		return instance;
+	}
+	private ProjectConfig(){}
+	
+	private String corpName;
+	private String projectName;
+	private String moduleName;
+	private String tablePrefix;
+	private String author;
+	private String dbPrefix;
+	private String projectPath;
+
+	public void loadProperties(){
+		try {
+//			Resource res = new ClassPathResource("project.properties");
+//			Properties p = new Properties();
+//			p.load(res.getInputStream());
+
+			File file = new File("project.properties");
+			InputStream in = new FileInputStream(file);
+			Properties p = new Properties();
+			p.load(in);
+			if(p.getProperty("corpname")!=null){
+				this.setCorpName(p.getProperty("corpname"));
+			}
+			if(p.getProperty("projectname")!=null){
+				this.setProjectName(p.getProperty("projectname"));
+			}
+			if(p.getProperty("modulename")!=null){
+				this.setModuleName(p.getProperty("modulename"));
+			}
+			if(p.getProperty("tableprefix")!=null){
+				this.setTablePrefix(p.getProperty("tableprefix"));
+			}	
+			if(p.getProperty("author")!=null){
+				this.setAuthor(p.getProperty("author"));
+			}
+			if(p.getProperty("dbprefix")!=null){
+				this.setDbPrefix(p.getProperty("dbprefix"));
+			}
+			if(p.getProperty("projectpath")!=null){
+				this.setProjectPath(p.getProperty("projectpath"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}	
+	
+	public String getCorpName() {
+		return corpName;
+	}
+	public void setCorpName(String corpName) {
+		this.corpName = corpName;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getModuleName() {
+		return moduleName;
+	}
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+	public String getTablePrefix() {
+		return tablePrefix;
+	}
+	public void setTablePrefix(String tablePrefix) {
+		this.tablePrefix = tablePrefix;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDbPrefix() {
+		return dbPrefix;
+	}
+
+	public void setDbPrefix(String dbPrefix) {
+		this.dbPrefix = dbPrefix;
+	}
+
+	public String getProjectPath() {
+		return projectPath;
+	}
+
+	public void setProjectPath(String projectPath) {
+		this.projectPath = projectPath;
+	}
+}
